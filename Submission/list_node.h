b13fc48def6d03e52e24cc7a1e35c607b463c7e4
Ty <ty.h.ahrens@gmail.com>
2024-10-23 15:33:58 -0500

    Started the development for ListNode class

--- Submission/list_node.h
+++ Submission/list_node.h
@@ -5,3 +5,28 @@
  *      PURPOSE: This class that is representing the other class for 
  *               program 2
  */
+
+#define LIST_NODE_H
+#ifndef LIST_NODE_H
+#include <iostream>
+#include <iomanip>
+
+template <typename T>
+class ListNode
+{
+	public:	
+		/*	notice the attributes are public so that the 
+			LinkedList class can directly access them */
+			
+		T value;        // Node value
+		ListNode *next; // Pointer to a ListNode object (the next node object)
+
+		// Constructor
+		ListNode (T value)
+		{ 
+			this->value = value;
+			this->next = NULL;
+		}
+};
+
+#endif
