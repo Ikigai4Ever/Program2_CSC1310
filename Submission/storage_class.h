2a98d81c89c83a7a408928d9a8ed7245329cf233
bowmanevan <cebowman42@tntech.edu>
2024-10-23 15:43:10 -0500

    added constrcuter for storage class

--- Submission/storage_class.h
+++ Submission/storage_class.h
@@ -1,5 +1,5 @@
 /*
- *      NAME:       Ty Ahrens
+ *      NAME:       Christopher Bowman
  *      PROGRAM:    storage_class.h
  *      DATE:       10/23/24
  *      PURPOSE:    This class is used as the linked list for 
@@ -8,40 +8,53 @@
 
 #define STORAGE_CLASS_H
 #ifndef STORAGE_CLASS_H
+#include "materials_class.h"
 #include <iostream>
 #include <iomanip>
 #include <string>
 
-template <typename T>
+
 class Storage{
     private:
-        struct StackNode{
-            T value;          // Value in the node
-            StackNode *next;  // Pointer to next node
-        };
         
-        StackNode *top;     // Pointer to the stack top
-        std::string materialType;
     
     public:
 
-    //functions
+    //functions (add to top, choose where to add, add to bottom, remove from top, choose where to remove, remove from bottom)
 
 
-    //constructor
-    DynamicStack();
+    //3 calls of the push function for mulch, sand, and clay
+    Storage() //color, cost, abundance, index
+    {
+        Material mulch;
+        mulch.setColor("Brown");
+        mulch.setCost(12.87); //dollars, float
+        mulch.setAbundance(13); //bags, int
+        mulch.setIndex(0); //where it is at in the list, int
+
+        Material sand;
+        sand.setColor("Beige");
+        sand.setCost(42.09); //dollars, float
+        sand.setAbundance(7); //bags, int
+        sand.setIndex(1); //where it is at in the list, int
+
+        Material clay;
+        clay.setColor("Blue");
+        clay.setCost(99.99); //dollars, float
+        clay.setAbundance(4); //bags, int
+        clay.setIndex(2); //where it is at in the list, int
+
+        push(mulch);
+        push(sand);
+        push(clay);
+    };
    
     //Destructor
-    ~DynamicStack();
+    ~Storage();
 
 };
 
-//****************** FUNCTIONS **********************/
 
-/*
- *      FUNCTION: getMaterialType()
- *      PURPOSE: getter function for material type
- */
 
 
 
